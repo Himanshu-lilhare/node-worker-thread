3adbdfb422836809da8d586d94add1bebd728bb0
Himanshu Lilhare <rajlilhare200@gmail.com>
2023-09-04 17:41:29 +0530

    without-worker-thread

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ import express from "express";
 
 const app = express();
 
+
+
 app.get("/", (req, res) => {
-  let response=0;
+  let response = 0;
   for (let i = 0; i < 100; i++) {
     response++;
   }
@@ -11,11 +13,18 @@ app.get("/", (req, res) => {
 });
 
 app.get("/longtask", (req, res) => {
-  let response=0;
-  for (let i = 0; i < 100000000000000; i++) {
-    response++;
-  }
-  res.json(response);
+let response = 0 
+
+for(let i=0;i<100000000000;i++){
+  response++
+}
+
+res.json(response)
+});
+
+
+app.get("/hello", (req, res) => {
+  res.json("hello");
 });
 
 app.listen(3000, () => {
