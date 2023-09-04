a24a12a2e733dd173ca2309ce69289e53be7face
Himanshu Lilhare <rajlilhare200@gmail.com>
2023-09-04 17:49:51 +0530

    calculate sun with woring htread

--- index.js
+++ index.js
@@ -1,32 +1,18 @@
 import express from "express";
-import { Worker, isMainThread, parentPort } from "worker_threads";
+import { Worker } from "worker_threads";
 const app = express();
 
+app.get("/calculatesum", (req, res) => {
+  let { num1, num2 } = req.query;
 
+  let worker = new Worker("./sumWorker.js");
 
-app.get('/calculatesum',(req,res)=>{
+  worker.postMessage({ num1, num2 });
 
-let {num1,num2} = req.query
-
-if(isMainThread){
-  let worker = new Worker('./sumWorker.js')
-
-  worker.postMessage({num1,num2})
-  
-  worker.on('message',(data)=>{
-    res.json(data)
-  })
-}else{
-  parentPort.on('message',(values)=>{
-    const {num1,num2} = values
-    parentPort.postMessage(parseInt(num1)+parseInt(num2))
-})
-}
-
-
-
-
-})
+  worker.on("message", (data) => {
+    res.json(data);
+  });
+});
 
 app.get("/", (req, res) => {
   let response = 0;
@@ -35,8 +21,6 @@ app.get("/", (req, res) => {
   }
   res.json(response);
 });
-
-
 
 app.get("/longtask", (req, res) => {
   let worker = new Worker("./worker.js");
